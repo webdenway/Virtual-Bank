2ef1ff1e98be7c3e0ff28d54b538ebdda7f72e2c
webdenway <clthbbd@gmail.com>
2020-07-29 09:57:13 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+    const loginBtn = document.getElementById("login-enter");
+    loginBtn.addEventListener("click", function() {
+        const loginArea = document.getElementById("login-area");
+        loginArea.style.display = "none";
+        const transactionArea = document.getElementById("transaction-area");
+        transactionArea.style.display = "block";
+    })
+    const depositAmount = document.getElementById("deposit-btn");
+    depositAmount.addEventListener("click", function() {
+
+    })
