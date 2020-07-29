554faafb39a4c9d9442114630d1657b007241729
webdenway <clthbbd@gmail.com>
2020-07-29 11:20:08 +0600

    first commit

--- script.js
+++ script.js
@@ -5,7 +5,25 @@
         const transactionArea = document.getElementById("transaction-area");
         transactionArea.style.display = "block";
     })
+
     const depositAmount = document.getElementById("deposit-btn");
     depositAmount.addEventListener("click", function() {
+        const inputDeposit = document.getElementById("input-deposit").value;
+        const inputNumber = parseFloat(inputDeposit);
+
+        const showDeposit = document.getElementById("show-input-deposit").innerText;
+        const depositEntry = parseFloat(showDeposit);
+
+        const showBalance = document.getElementById("show-main-balance").innerText;
+        const mainBalance = parseFloat(showBalance);
+
+
+        const totalDeposit = inputNumber + depositEntry;
+
+        const totalMainBalance = mainBalance + inputNumber;
+
+        document.getElementById("show-main-balance").innerText = totalMainBalance;
+        document.getElementById("show-input-deposit").innerText = totalDeposit;
+
 
     })
