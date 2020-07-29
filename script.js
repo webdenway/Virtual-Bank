522113e1cac1811e3150d9eb2c19e198cd75c70f
webdenway <clthbbd@gmail.com>
2020-07-29 12:42:07 +0600

    first commit

--- script.js
+++ script.js
@@ -24,6 +24,28 @@
 
         document.getElementById("show-main-balance").innerText = totalMainBalance;
         document.getElementById("show-input-deposit").innerText = totalDeposit;
+        document.getElementById("input-deposit").value = "";
 
 
+    })
+
+    const withdrawDeposit = document.getElementById("withdraw-btn");
+    withdrawDeposit.addEventListener("click", function() {
+        const withdrawAmount = document.getElementById("enter-withdraw-amount").value;
+        const inputWithdraw = parseFloat(withdrawAmount);
+
+        const showWithdraw = document.getElementById("show-withdraw-amount").innerText;
+        const currentWithdraw = parseFloat(showWithdraw);
+
+
+        const showBalance = document.getElementById("show-main-balance").innerText;
+        const mainBalance = parseFloat(showBalance);
+
+        const afterWithdrawBalance = mainBalance - inputWithdraw;
+
+        const totalWithdraw = inputWithdraw + currentWithdraw;
+
+        document.getElementById("show-main-balance").innerText = afterWithdrawBalance;
+        document.getElementById("show-withdraw-amount").innerText = totalWithdraw;
+        document.getElementById("enter-withdraw-amount").value = "";
     })
